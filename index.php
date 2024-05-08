f3e07e990f2219301c743e4cbece9bad806f6a34
Matt Miss <matt.w.miss@gmail.com>
2024-05-08 19:40:17 -0400

    added .htaccess and index.php with default route

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// SDEV328/application/index.php
+// This is my controller
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+
+
+// Define a default route
+$f3->route('GET /', function() {
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run fat free
+$f3->run();
